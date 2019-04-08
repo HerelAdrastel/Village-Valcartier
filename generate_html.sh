494b8c45f237b7483f84f419d2e7698a8a3373d2
Thomas de Lachaux <thomas.delachaux@gmail.com>
2019-04-07 23:00:34 -0400

    Moved to no php

---
+++ generate_html.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source='public_html/template.html'
+dest='public_html/index.html'
+
+echo "Fichier source: $source"
+echo "Fichier destinataire $dest"
+
+echo '' > $dest
+
+# Debut
+line_to_replace=`grep -n '<!-- IMAGES -->' $source | cut -d: -f 1`
+head -n $line_to_replace $source >> $dest
+
+# Generation de l'HTML
+for image in `ls public_html/images/sortie`
+do
+    image='images/sortie/'$image
+    tag='<a href="'$image'" data-lightbox="Gallery"><img src="'$image'" alt="Valcartier Image"></a>'
+    echo '                '$tag >> $dest
+done
+
+# Fin de l'html
+tail -n +$(($line_to_replace + 1)) $source >> $dest
+echo "Terminé !"
